61849a8e299d4fc768e02c998c4caa92b1269094
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-09-30 21:53:56 +0530

    add one question

---
+++ ArrayList/PairSumTwo.java
@@ -0,0 +1,20 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class PairSumTwo {
+    public static boolean pairSum(ArrayList<Integer> list, int target){
+        
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+         int target =5; 
+            System.out.println(pairSum(list, target));
+         
+    }
+    
+}
---
+++ ArrayList/find.java
@@ -0,0 +1,7 @@
+package ArrayList;
+
+public class find {
+    public static void main(String[] args) {
+        int
+    }
+}
