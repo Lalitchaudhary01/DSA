1fa1b0bde9286309479ed268eec37ed793879411
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-10-18 22:11:58 +0530

    add one question

--- ArrayList/find.java
+++
@@ -1,7 +0,0 @@
-package ArrayList;
-
-public class find {
-    public static void main(String[] args) {
-        int
-    }
-}
---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,7 @@
+package LinkedList;
+
+public class LinkedList {
+    System.out.println("Hello");
+
+    
+}
