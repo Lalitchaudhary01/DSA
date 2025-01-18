e890c130685e228080ccd9f42d11b422bfebcb4d
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2025-01-18 11:46:20 +0530

    linkedlist

--- ArrayList/PairSumTwo.java
+++ ArrayList/PairSumTwo.java
@@ -2,8 +2,29 @@ package ArrayList;
 import java.util.ArrayList;
 
 public class PairSumTwo {
-    public static boolean pairSum(ArrayList<Integer> list, int target){
-        
+    public static boolean pairSum2(ArrayList<Integer> list, int target){
+        int bp = -1;
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i)> list.get(i+1)){
+                bp = i;
+                break;
+            }
+        }
+        int lp = bp+1;
+        int rp = bp;    
+
+        while(lp != rp){
+            //case1
+            if(list.get(lp) + list.get(rp) == target){
+                return true;
+            }
+            //case2
+            else if(list.get(lp) + list.get(rp) < target){
+                lp = (lp+1) % list.size();
+            }else{
+                rp = (rp-1+list.size()) % list.size();
+            }
+        }
     }
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
@@ -13,7 +34,7 @@ public class PairSumTwo {
         list.add(4);
         list.add(5);
          int target =5; 
-            System.out.println(pairSum(list, target));
+            System.out.println(pairSum2(list, target));
          
     }
     
--- LinkedList/PrintLinkedList.java
+++ LinkedList/PrintLinkedList.java
@@ -1,4 +1,4 @@
-public class LinkedList{
+public class PrintLinkedList {
     public static class Node{
         int data;
         Node next;
@@ -53,13 +53,48 @@ public class LinkedList{
         //step3
         tail = newNode;
     }
+    public static void printLinkedList(){
+        if(head == null){
+            System.out.println("Linked List is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    //Add in the middle
+    //step 1
+    //create a new node
+    //step 2
+    //find the middle node
+    //step 3
+    //new node's next = middle.next
+    //step 4
+    //middle.next = new node
+    public void addMiddle(int data){
+        //step1
+        Node newNode = new Node(data);
+        //step2
+        Node slow = head;
+        Node fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //step3
+        newNode.next = slow.next;
+        //step4
+        slow.next = newNode;
+    }
     public static void main(String args[]){
-        LinkedList ll = new LinkedList();
+        PrintLinkedList ll = new PrintLinkedList();
         ll.aadFirst(20);
         ll.aadFirst(10);
         ll.addLast(30);
-        ll.addLast(40);
-      
-
+        printLinkedList();
     }
+    
 }
