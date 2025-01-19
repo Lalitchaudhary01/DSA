024f27ad01b2fa4fb3c9a665749b48b1a1b731f7
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2025-01-19 08:53:26 +0530

    linkedlist

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -53,12 +53,102 @@ public class LinkedList{
         //step3
         tail = newNode;
     }
+
+    //remove first
+    //head = head.next
+    public int  removeFirst(){
+            if(size == null){
+                System.out.println("Linked List is empty");
+                return -1;
+            } else if(size ==1){
+                int val = head.data;
+                head = null;
+                tail = null;
+                return val;
+            }
+       int val = head.data;
+         head = head.next;
+         return val;
+    }
+    //remove last
+    //tail = null
+    //tail = tail.next
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("Linked List is empty");
+            return -1;
+        } else if(size == 1){
+            int val = head.data;
+            head = null;
+            tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        for(int i=0; i<size-2; i++){
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+    //remove at index
+    //if index == 0
+    //remove first
+    //if index == size-1
+    //remove last
+    //else
+    //prev = head
+    //for i = 0 to index-2
+    //prev = prev.next
+    //prev.next = prev.next.next
+    public int removeAt(int index){
+        if(index < 0 || index >= size){
+            System.out.println("Invalid index");
+            return -1;
+        }
+        if(index == 0){
+            return removeFirst();
+        } else if(index == size-1){
+            return removeLast();
+        } else {
+            Node prev = head;
+            for(int i=0; i<index-1; i++){
+                prev = prev.next;
+            }
+            int val = prev.next.data;
+            prev.next = prev.next.next;
+            size--;
+            return val;
+        }
+    }
+    // search for a key in linked list
+    // if key is found return true
+    // else return false
+    public int itrSearch(int key){
+        Node temp = head;
+        int index = 0;
+        while(temp != null){
+            if(temp.data == key){
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        return -1;
+    }
+
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
         ll.aadFirst(20);
         ll.aadFirst(10);
         ll.addLast(30);
         ll.addLast(40);
+        System.out.println(ll.removeFirst());
+        
+        
       
 
     }
---
+++ LinkedList/Size.java
@@ -0,0 +1,11 @@
+public class Size {
+    
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list.size());
+    }
+}
